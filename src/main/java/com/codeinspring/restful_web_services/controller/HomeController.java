9de46dc3739b7a0d138f47036769ed75ef725553
subhamk <subhamk@juniper.net>
2023-02-25 17:49:18 +0530

    Path Variables

--- src/main/java/com/codeinspring/restful_web_services/controller/HomeController.java
+++ src/main/java/com/codeinspring/restful_web_services/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.codeinspring.restful_web_services.controller;
 
 import com.codeinspring.restful_web_services.Bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,11 @@ public class HomeController {
     @GetMapping("/welcome-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    // hello-world/path-variable/in40minutes
+    @GetMapping("/welcome/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
